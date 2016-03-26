d553ef03d08dcdfd1e333ba7c2ae95d5d7e9bc76
Denis Andreev <wiirlock@gmail.com>
2016-03-26 19:53:48 +0300

    Initial Commit

---
+++ WMidiPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMidiPlayer
+//
+//  Created by Denis Andreev on 26.03.16.
+//  Copyright © 2016 Denis Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
